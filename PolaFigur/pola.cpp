1e9b72899b40f5e95fbd2834e01040a502efe945
ormo91 <patryk_141@vp.pl>
2019-10-30 23:31:33 +0100

    changed language and corrected formatting

--- PolaFigur/pola.cpp
+++ PolaFigur/pola.cpp
@@ -3,147 +3,138 @@
 #define PI 3.14
 
 
-void menuBox (); 		
-void menuDecision (int);
-double poleKola (double);
-double poleKwadratu (double);
-double poleProstokata (double,double);
-double poleTrojkata (double,double);
-bool jestDobrze (std::string);
-bool jestDobrze ();
+void MenuBox (); 		
+void MenuDecision (int);
+void CircleArea (double);
+void SquareArea (double);
+void RectangleArea (double,double);
+void TriangleArea (double,double);
+bool IsRight (std::string);
 
 int main ()
 {
-    int wybor;
-    char zn;
+    int option;
+    char ch;
 
     do
     {
-    std::system ("clear");
+      std::system ("clear");
+      MenuBox (); 
+    while(! (std::cin >> option))
+    {
+      std::cin.clear();
+      std::cin.ignore (1000, '\n');
+      std::system ("clear");  
+      MenuBox ();
+      std::cout << "You have cartainly not choosen a value beetwen 1-4." << std::endl;
+    }
 
-    menuBox (); 
-
-    while (!(std::cin >> wybor))
-	  {
-	  std::cin.clear();
-	  std::cin.ignore (1000, '\n');
-	  std::system ("clear");
-	  
-	  menuBox ();
-	  std::cout << "Na pewno nie podales wartosci z przedzialu 1-4" << std::endl;
-          }
-    
-
-    menuDecision (wybor);
+    MenuDecision (option);
 
     do
-	{
-	    std::cout << "Czy chcesz kontynuowac program? (T/N)" << std::endl;
-	    std::cin >> zn;
-	    std::cin.ignore (2222, '\n');
-	}while (zn != 't' && zn != 'T' && zn != 'n' && zn != 'N');
-    }while (zn == 't' || zn == 'T');    
-
-	
+    {
+      std::cout << "Do you wanna continue this program? (Y/N)" << std::endl;
+      std::cin >> ch;
+      std::cin.ignore (2222, '\n');
+    }
+    while (ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n');
+    }
+    while (ch == 'Y' || ch  == 'y');    
+    
 return 0;
 }
 
-
-void menuBox ()
+void MenuBox ()
 {
-    std::cout<<"Wybierz opcje"<<std::endl;
-    std::cout<<"1.Pole Kola"<<std::endl;
-    std::cout<<"2.Pole Kwadratu"<<std::endl;
-    std::cout<<"3.Pole Prostokata"<<std::endl;
-    std::cout<<"4.Pole Trojkata"<<std::endl;
+    std::cout << "Select option" << std::endl;
+    std::cout << "1.Circle area" << std::endl;
+    std::cout << "2.Square area" << std::endl;
+    std::cout << "3.Rectangle area" << std::endl;
+    std::cout << "4.Triangle area" << std::endl;
 }
 
 
-void menuDecision (int wybor)
+void MenuDecision (int option)
 {
-    switch (wybor)
+    switch (option)
     {
 	int a, b;
 	case 1:
-	    do
-	    {
-	    std::cout << "Podaj promien: " << std:: endl;
+	  do
+	  {
+	    std::cout << "Enter the radius: " << std:: endl;
 	    std::cin>>a;
-	    }
-	    while (!jestDobrze("zle, podales niepoprawna dana\n" ));
-	    poleKola (a);
-	    break;
+	  }
+	  while(!IsRight ("Wrong, you provided an invalid data"));
+	  CircleArea (a);
+	  break;
 	case 2:
-            do
-            {
-            std::cout << "Podaj bok kwardratu: " << std:: endl;
+          do
+          {
+            std::cout << "Enter the side of square: " << std:: endl;
             std::cin >> a;
-            }
-            while (!jestDobrze("zle, podales niepoprawna dana\n" ));
-
-	    poleKwadratu (a);
+          }
+          while(!IsRight ("Wrong, you provided an invalid data"));
+	    SquareArea (a);
             break;
 	case 3:
             do
             {
-            std::cout << "Podaj boki prostokata: " << std:: endl;
-            std::cin >> a >> b ;
+              std::cout << "Enter sides of rectangle: " << std:: endl;
+              std::cin >> a >> b ;
             }
-            while (!jestDobrze("zle, podales niepoprawna dana\n" ));
-	    poleProstokata (a, b);
+            while(!IsRight ("Wrong, you provided an invalid data" ));
+	    RectangleArea (a, b);
             break;
 	case 4:
             do
             {
-            std::cout << "Podaj podstawe i wysokosc trojkata: " << std:: endl;
-            std::cin >> a >> b;
+              std::cout << "Enter : " << std:: endl;
+              std::cin >> a >> b;
             }
-            while (!jestDobrze ("zle, podales niepoprawna dana\n " ));
-	    
-	    poleTrojkata (a,b);
+            while(!IsRight ("Wrong, you provided an invalid data"));
+	    TriangleArea (a,b);
 	    break;
 	default:
-	    std::cout << "Na pewno nie wybrales odpowiedniej opcji z menu." << std::endl;
+	    std::cout << "You hadn't certainly choose right option" << std::endl;
     }
 }
 
 
-double poleKola (double a)
+void CircleArea (double a)
 {
-    double wynik = PI * a * a;
-
-    std::cout << "Pole kola wynosi: " << wynik << std::endl;
+    double result = PI * a * a;
+    std::cout << "Circle area is equal: " << result << std::endl;
 }
 
-double poleKwadratu (double a)
+void SquareArea (double a)
 {
-    double wynik = a * a;
-
-    std::cout << "Pole kwadratu wynosi: " << wynik << std::endl;
-}
-double poleProstokata (double a, double b)
-{
-    double wynik = a * b;
-
-    std::cout << "Pole prostokata wynosi: " << wynik << std::endl;
-}double poleTrojkata (double a, double b)
-{
-    double wynik = 0.5 * a * b;
-
-    std::cout << "Pole trojkata wynosi: " << wynik << std::endl;
+    double result = a * a;
+    std::cout << "Square area is equal: " << result << std::endl;
 }
 
-bool jestDobrze(std::string error_msg)
+void RectangleArea (double a, double b)
+{
+    double result = a * b;
+    std::cout << "Rectangle area is equal: " << result << std::endl;
+}
+
+void TriangleArea (double a, double b)
+{
+    double result = 0.5 * a * b;
+    std::cout << "Triangle area is equal: " << result << std::endl;
+}
+
+bool IsRight(std::string error_msg)
 {
     if (std::cin.rdstate())
-	{
-	std::cin.clear();
-	std::cin.ignore(1000, '\n');
-
-	std::system ("clear");
-	menuBox();
-	std::cout << error_msg;
-	return false;
-	}
+    {
+      std::cin.clear();
+      std::cin.ignore(1000, '\n');
+      std::system ("clear");
+      MenuBox();
+      std::cout << error_msg;
+      return false;
+    }
     return true;
 }
